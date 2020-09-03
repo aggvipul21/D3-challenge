fda690612d09fa9780c1064bc01872da60ccac9b
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-09-03 08:40:21 -0400

    Added code to insert text in circles of scatterplot

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,9 +22,6 @@ var chartGroup = svg.append("g")
 d3.csv("assets/data/data.csv").then(censusdata=> {
   console.log(censusdata);
  
-  // Create a function to parse date and time
-  var parseTime = d3.timeParse("%d-%b-%Y");
-
   // Format the data
   censusdata.forEach(data=> {
     data.healthcare = +data.healthcare;
@@ -35,11 +32,11 @@ d3.csv("assets/data/data.csv").then(censusdata=> {
 
   // Create scaling functions
   var xLinearScale1 = d3.scaleLinear()
-    .domain(d3.extent(censusdata, d => d.poverty))
+    .domain([d3.min(censusdata, d => d.poverty)*0.8,d3.max(censusdata, d => d.poverty)*1.2])
     .range([0, width]);
 
   var yLinearScale1 = d3.scaleLinear()
-    .domain(d3.extent(censusdata, d => d.healthcare))
+  .domain([d3.min(censusdata, d => d.healthcare)*0.8,d3.max(censusdata, d => d.healthcare)*1.2])
     .range([height, 0])
 //   var yLinearScale2 = d3.scaleLinear()
 //     .domain([0, d3.max(smurfData, d => d.smurf_sightings)])
@@ -50,16 +47,15 @@ d3.csv("assets/data/data.csv").then(censusdata=> {
   var leftAxis = d3.axisLeft(yLinearScale1);
 //   var rightAxis = d3.axisRight(yLinearScale2);
 
-  // Add x-axis
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+// Add x-axis
+chartGroup.append("g")
+.attr("transform", `translate(0, ${height})`)
+.call(bottomAxis);
 
-  // Add y1-axis to the left side of the display
-  chartGroup.append("g")
-    // Define the color of the axis text
-    .classed("green", true)
-    .call(leftAxis);
+// Add y1-axis to the left side of the display
+chartGroup.append("g")
+// Define the color of the axis text
+ .call(leftAxis);
 
   // Add y2-axis to the right side of the display
 //   chartGroup.append("g")
@@ -68,16 +64,28 @@ d3.csv("assets/data/data.csv").then(censusdata=> {
 //     .attr("transform", `translate(${width}, 0)`)
 //     .call(rightAxis);
 
-     var circlesGroup = chartGroup.selectAll("circle")
+var circlesGroup = chartGroup.selectAll("circle")
     .data(censusdata)
     .enter()
     .append("circle")
     .attr("cx", d => xLinearScale1(d.poverty))
     .attr("cy", d => yLinearScale1(d.healthcare))
-    .attr("r", "10")
-    .attr("fill", "gold")
+    .attr("r", "15")
+    .attr("fill", "steelblue")
     .attr("stroke-width", "1")
-    .attr("stroke", "black");
+    .attr("stroke", "black")
+    
+  chartGroup.selectAll("abbreviation")
+    .data(censusdata)
+    .enter()
+    .append("text")
+    .attr("x", d=>xLinearScale1(d.poverty))
+    .attr("y", d=>yLinearScale1(d.healthcare))
+    .text(d=> d.abbr)
+    .attr("font-family", "sans-serif")
+    .attr("font-size","10px")
+    .attr("text-anchor","middle")
+    .attr("fill", "white");
 
 //   var line2 = d3.line()
 //     .x(d => xTimeScale(d.date))
@@ -92,12 +100,15 @@ d3.csv("assets/data/data.csv").then(censusdata=> {
 
   // Append axes titles
   chartGroup.append("text")
-  .attr("transform", `translate(${width / 2}, ${height + margin.top + 20})`)
-    .classed("text", true)
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
     .text("Lacks Healthcare(%)");
 
   chartGroup.append("text")
-  .attr("transform", `translate(${width / 2}, ${height + margin.top + 37})`)
+  .attr("transform", `translate(${width / 2}, ${height + margin.top + 10})`)
     .classed("text", true)
     .text("In Poverty (%)");
 }).catch(function(error) {
